2a907ee1c85a7098d0ae52556ad77e8780a73682
anushkaraheja <Anushka@D-173-250-158-204.dhcp4.washington.edu>
2017-10-05 14:33:50 -0700

    welp

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -7,12 +7,12 @@ my.name <- "Anushka"
 # Assign your height to a variable `my.height`
 my.height <- "65 inches"
 # Create a variable `puppies` equal to the number of puppies you'd like to have
-puppies <- "6"
+puppies <- 6
 # Create a variable `puppy.price`, which is how expensive you think a puppy is
-puppy.price <- "$400"
+puppy.price <- 400
 # Create a variable `total.cost` that has the total cost of all of your puppies
-total.cost <- "$2400"
+total.cost <- 2400
 # Create a boolean variable `too.expensive`, set to true if the cost is greater than $1,000
 too.expensive <- TRUE
 # Create a variable `max.puppies`, which is the nuber of puppies you can afford for $1K.
-max.puppies <- "3"
+max.puppies <- 3
